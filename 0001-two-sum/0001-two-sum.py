f058020e330de4c3d2cf2f25c877ae79472c81ab
Ayush tamboli <ayushtamboli708@gmail.com>
2026-08-20 22:45:39 +0530

    Time: 1 ms (61.62%), Space: 13.1 MB (67.62%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,15 +1,15 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        seen = {}
-
+        seen = {} # Stores {value: index}
+        
         for i, num in enumerate(nums):
             complement = target - num
+            
+            # Check if the complement is already in our map
             if complement in seen:
-                return [seen[complement], i]
-
+                return [seen[complement], i] # Found it! Return indices
+            
+            # Store current number and its index for future matches
             seen[num] = i
+            
+        return None # No solution found
